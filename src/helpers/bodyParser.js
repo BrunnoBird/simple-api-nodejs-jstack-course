8aa2568d3823492a92727a4e3cd7fdd82eb0627a
brunnobird <bird.tec.gonzalez@gmail.com>
2021-09-22 10:03:47 -0300

    feat: Comments on the project for study

--- src/helpers/bodyParser.js
+++ src/helpers/bodyParser.js
@@ -16,3 +16,12 @@ function bodyParser(request, callback) {
   }
 
 module.exports = bodyParser;
+
+/* MARK1
+ Fica ouvindo as mensagens ( sempre que chega um evento com data "partezinha do body")
+  vamos concatenando ele dentro de uma let Body
+  e quando chegar a ultima menssagem chamamos o metodo 'END' transformamos o nosso BODY
+  e um OBJ Json
+  passamos o OBJ transformado dentro de request.body e chamamos o nosso callback()
+  que passamos quando executamos a funcao bodyParsers
+*/
--- src/index.js
+++ src/index.js
@@ -15,12 +15,13 @@ const server = http.createServer((request, response) => {
 
   const splitEndPoint = pathname.split('/').filter(Boolean); //Mark2
 
+  // MARK3 
   if(splitEndPoint.length > 1) {
     pathname = `/${splitEndPoint[0]}/:id`; // [0] = Endpoint
     id = splitEndPoint[1]; // [1] = ID
   }
   
-
+  // MARK4
   const route = routes.find((routeObjt) => (
     routeObjt.endpoint === pathname && routeObjt.method === request.method
   ));
@@ -29,11 +30,13 @@ const server = http.createServer((request, response) => {
     request.query = Object.fromEntries(parseUrl.searchParams); //Mark1
     request.params = { id };
 
+    // MARK5
     response.send = (statusCode, body) => {
       response.writeHead(statusCode, { 'Context-Type': 'application/json' });
       response.end(JSON.stringify(body));
     }
 
+    // MARK6
     if(['POST', 'PUT', 'PATCH'].includes(request.method)){
       bodyParser(request, () => route.handler(request, response));
     } else {
@@ -52,10 +55,37 @@ server.listen(3000, () => console.log(' 🔥 Server started at http://localhost:
   Convertendo o tipo Iterable em Objt JS
   Iterable Type -> parseUrl.searchParams
   request.query = Object.fromEntries(parseUrl.searchParams);
+  OBS: Injecao de QUERRY PARAMS e ID
 */
 
 /* Mark2 
   Truefy and Falsy do JS -> Caso meu filter retorne algum valor falsy ele nao coloca
     sendo assim '' = falsy nao aparece no console.log
     -> filter((routeItem) => Boolean(routeItem)) <-
+*/
+
+/* MARK3
+  Split na minha rota para conseguirmos separar oque e 
+    Endpoint e o oque e query Param ( no caso o ID ) do User
+    e posteriormente injetamos o ID salvo dentro da ROUTES no
+    request.params = { id }
+ */
+
+/* MARK4
+  Identificando o MATCH tanto de endpoint(PATH name), como de metodo(request)
+*/
+
+/* MARK5
+  Criando um send que consigo reaproveitar em varios lugares do codigo
+    evitando escrita repetitiva do writeHead e do End
+*/
+
+/* MARK6
+  Verificando o metodo HTTP que estamos enviando, e se tiver executamos a funcao
+    bodyParser para conseguir pegar o string do Body e transformar em um OBJ Json
+    e como ele vai chegando aos poucos, temos que passar uma funcao de CallBack
+    no nosso arquivo bodyParser.js
+
+    A Funcao de Callback acaba sendo a funcao dos nossos HANDLERS que definimos
+    dentro de nossas rotas.
 */
